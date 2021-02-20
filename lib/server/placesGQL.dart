f3f377a889f0222ef6db644ce4fa225191a04327
Harmanjit Singh <harmanjit140500@gmail.com>
2021-02-20 11:19:03 +0530

    job query

---
+++ lib/server/placesGQL.dart
@@ -0,0 +1,39 @@
+import 'package:graphql/client.dart';
+
+Future getAllJobs() async {
+  final _httpLink = HttpLink(
+    'https://loca-ly.herokuapp.com/api',
+  );
+
+  final _authLink = AuthLink(
+    getToken: () async =>
+        'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Imd1cmxlZW5Abm90Tm9vYi5jb20iLCJleHAiOjE2MTM3OTc4NTUsIm9yaWdJYXQiOjE2MTM3OTc1NTV9.wnHIalgMxH9xda2Sl4eAcBDN8GCHBWMvd0nGdG-Zb24',
+  );
+
+  Link _link = _authLink.concat(_httpLink);
+
+  final GraphQLClient client = GraphQLClient(
+    /// **NOTE** The default store is the InMemoryStore, which does NOT persist to disk
+    cache: GraphQLCache(),
+    link: _link,
+  );
+
+  const String readRepositories = """
+   {
+  alljobs{
+    title
+  }
+}
+""";
+  final QueryOptions options = QueryOptions(
+    document: gql(readRepositories),
+  );
+
+  final QueryResult result = await client.query(options);
+
+  if (result.hasException) {
+    print(result.exception.toString());
+  } else {
+    print("HAS DATA");
+  }
+}
